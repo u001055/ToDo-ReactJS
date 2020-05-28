83a705ff1a30a31250c25de35809e1f6c09734e6
Oleg <52830599+u001055@users.noreply.github.com>
2020-05-28 15:26:42 +0300

    separation of components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 
-const el = (
-  <div>
-    <h1>My Todo List</h1>
-    <input placeholder='search' />
-    <ul>
-      <li>Learn React</li>
-      <li>Build Awesome App</li>
-    </ul>
-  </div>
-);
+const App = () => {
 
-ReactDOM.render(el, document.getElementById('root'));
+  const todoData = [
+    {label: 'Drink Coffee', important: false, id: 1 },
+    {label: 'Make Awesome App', important: true, id: 2 },
+    {label: 'Have a lunch', important: false, id: 3 }
+  ];
+
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />      
+      <TodoList todos={todoData}/>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
